693a57cc3a66f7a56cca054476f535c1afede2b2
Igor Novikov <igor.novikov@umbrellait.com>
2021-07-18 22:03:38 +0300

    lifecycle callback methods has been added

--- life_cycle/src/main/java/com/example/activities/MainActivity.kt
+++ life_cycle/src/main/java/com/example/activities/MainActivity.kt
@@ -1,11 +1,68 @@
 package com.example.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i(TAG, "onCreate: ")
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            startActivity(Intent(this,SecondActivity::class.java))
+        }
+    }
+
+
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart: ")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.i(TAG, "onRestoreInstanceState: ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume: ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause: ")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.i(TAG, "onSaveInstanceState: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop: ")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart: ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy: ")
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
